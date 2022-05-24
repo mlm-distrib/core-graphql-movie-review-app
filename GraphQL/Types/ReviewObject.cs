ab4290f829678068b1a96d41c7636e31e62afb10
Royson Paul <Roysonpaul.Immanuel@valgenesis.com>
2022-05-24 22:41:33 +0530

    initial commit

---
+++ GraphQL/Types/ReviewObject.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using MovieReviews.Models;
+
+namespace MovieReviews.GraphQL.Types
+{
+    public sealed class ReviewObject : ObjectGraphType<Review>
+    {
+        public ReviewObject()
+        {
+            Name = nameof(Review);
+            Description = "A review of the movie";
+
+            Field(r => r.Reviewer).Description("Name of the reviewer");
+            Field(r => r.Stars).Description("Star rating out of five");
+            Field(r => r.Comments).Description("Enter comments");
+        }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MovieReviews.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public Guid MovieId { get; set; }
+        public string Reviewer { get; set; }
+        public int Stars { get; set; }
+        public string Comments { get; set; }
+    }
+}
